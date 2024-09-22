46eabec1fa15e6f1541676a6b1aff64fef91d04a
Alexcel-Harry <alexcel.harry@sjtu.edu.cn>
2024-09-22 16:36:56 -0400

    Completed: part 1

--- __jac_gen__/server.py
+++ __jac_gen__/server.py
@@ -17,3 +17,24 @@ class interact_with_body(_Jac.Walker):
 
     def return_message(self, _jac_here_: _Jac.RootType) -> None:
         _Jac.report({'response': 'Hello, ' + self.name + '!'})
+
+@_Jac.make_walker(on_entry=[_Jac.DSFunc('return_message', _Jac.RootType)], on_exit=[])
+@__jac_dataclass__(eq=False)
+class new_interact(_Jac.Walker):
+    name: str
+    time: bool
+
+    def return_message(self, _jac_here_: _Jac.RootType) -> None:
+        if self.time:
+            _Jac.report({'response': 'Good morning, ' + self.name + '!'})
+        else:
+            _Jac.report({'response': 'Good evening, ' + self.name + '!'})
+
+@_Jac.make_walker(on_entry=[_Jac.DSFunc('return_message', _Jac.RootType)], on_exit=[])
+@__jac_dataclass__(eq=False)
+class new_new_interact(_Jac.Walker):
+    a: int
+    b: int
+
+    def return_message(self, _jac_here_: _Jac.RootType) -> None:
+        _Jac.report({'response': 'difference = ' + str(self.a - self.b)})
